08bc494c29e2ebdd73d5f4ab4be917cd3843471a
Evan Brass <brassevan@gmail.com>
2024-01-04 10:08:07 -0800

    _ Parse connection tests

--- bind/main.js
+++ bind/main.js
@@ -3,7 +3,9 @@ import { Turn } from "./turn.js";
 
 const websockets = new Set();
 Deno.serve(req => {
+	console.log(req);
 	const {socket, response} = Deno.upgradeWebSocket(req);
+	console.log(response);
 	websockets.add(socket);
 	socket.addEventListener('close', () => websockets.delete(socket));
 
@@ -13,7 +15,19 @@ Deno.serve(req => {
 // Broadcast the TURN messages to the WebSockets
 const hub = await Deno.connect({ hostname: 'hub', port: 3478 });
 for await (const msg of Turn.parse(hub.readable)) {
+	// We only care about data indications
+	if (msg.class != 0b01) continue;
+	if (msg.method != 0x007) continue;
+	// We only want ICE connection tests (STUN Bind with username)
+	if (msg.data?.byteLength < 20) continue;
+	const inner = new Turn(msg.data.buffer, msg.data.byteOffset, msg.data.byteLength);
+	if (inner.class != 0b00) continue;
+	if (inner.method != 0x001) continue;
+	const username = inner.username;
+	const peer = msg.xpeer;
+	if (!username || !peer) continue;
 	for (const ws of websockets) {
-		ws.send(msg);
+		if (ws.readyState != 1) continue;
+		ws.send(JSON.stringify({username, peer}));
 	}
 }
