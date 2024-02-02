9d9856e2fef8476a1f668a2de7bbf12f0a3324b3
Evan Brass <brassevan@gmail.com>
2024-02-02 11:59:39 -0800

    _

--- stun/src/rfc8489.rs
+++ stun/src/rfc8489.rs
@@ -7,43 +7,40 @@ pub struct StunError<'i> {
 	reason: &'i str,
 	unknown_attrs: Vec<u16>
 }
-pub enum StunUser<'i> {
-	NoUser,
-	ShortTerm {
-		username: &'i str
-	},
-	LongTerm {
-		realm: &'i str,
-		username: &'i str
-	}
-}
-pub trait StunAuth {
-	type Key: AsRef<[u8]>;
-	fn key(&self) -> Option<Self::Key>;
-}
 
-pub struct Rfc8489<'i, A: StunAuth, N> {
-	authorizer: A,
-
+pub struct Rfc8489<'i, N> {
 	mapped: Option<SocketAddr>,
-	software: &'i str,
-
-	auth: StunUser<'i>,
-	nonce: &'i str,
-	integrity: Option<A::Key>,
-	fingerprint: bool,
-	
+	username: &'i str,
+	integrity: bool,
 	error: Option<StunError<'i>>,
+	realm: &'i str,
+	nonce: &'i str,
+	software: &'i str,
 	alternate: Option<(SocketAddr, &'i str)>,
+	fingerprint: bool,
 
 	next: N
 }
 pub enum Rfc8489Error<E> {
 	Next(E)
 }
-impl<'i, A: StunAuth, N: StunAttrs<'i>> StunAttrs<'i> for Rfc8489<'i, A, N> {
+impl<'i, N: StunAttrs<'i>> StunAttrs<'i> for Rfc8489<'i, N> {
 	type Error = Rfc8489Error<N::Error>;
-	fn decode_attr(&mut self, header: &[u8; 20], prefix: &[u8], typ: u16, value: &'i [u8]) -> Result<(), Self::Error> {
-		todo!()
+	fn decode_attr(&mut self, header: &[u8; 20], attr_prefix: &[u8], attr_typ: u16, value: &'i [u8]) -> Result<(), Self::Error> {
+		match attr_typ {
+			0x0001 /* MAPPED-ADDRESS */=> {}
+			_ if self.fingerprint => {}, // Ignore all attributes after the fingerprint
+			0x8028 /* FINGERPRINT */ => {
+
+			}
+			_ if self.integrity => {}, // Ignore attributes after the integrity (except fingerprint)
+			0x0006 /* USERNAME */ => {
+				self.username = std::str::from_utf8(value).unwrap_or_else(op);
+			}
+			todo!()
+
+			_ => return self.next.decode_attr(header, attr_prefix, attr_typ, value).map_err(Rfc8489Error::Next)
+		}
+		Ok(())
 	}
 }
