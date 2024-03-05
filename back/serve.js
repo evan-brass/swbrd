cc4e82490820692ff0974f1b0ca97462c139777b
Evan Brass <brassevan@gmail.com>
2024-03-05 14:33:41 -0800

    _

--- back/serve.js
+++ back/serve.js
@@ -1,5 +1,7 @@
 import { serveDir } from '@std/http/file_server.ts';
 import { Stun, Class, Method } from '../src/stun.js';
+import { encoder } from "../src/stun.js";
+import { md5 } from "../src/md5.js";
 
 Deno.serve(req => serveDir(req, {
 	fsRoot: './',
@@ -7,26 +9,26 @@ Deno.serve(req => serveDir(req, {
 	showDirListing: true
 }));
 
+const short_key = await crypto.subtle.importKey('raw', encoder.encode("the/ice/password/constant"), {
+	name: 'HMAC',
+	hash: 'SHA-1'
+}, true, ['sign', 'verify']);
+const long_key = await crypto.subtle.importKey('raw', md5("guest:realm:the/guest/password"), {
+	name: 'HMAC',
+	hash: 'SHA-1'
+}, true, ['sign', 'verify']);
+
+const resp = new Stun(new ArrayBuffer(20, { maxByteLength: 4096 }))
 for await (const [packet, addr] of Deno.listenDatagram({ transport: 'udp', hostname: '::', port: 3478 })) {
 	if (packet.byteLength < 20) continue;
-
 	const msg = new Stun(packet.buffer, packet.byteOffset, packet.byteLength);
 
-	if (msg.method == Method.binding) {
-		if (msg.class != Class.request) continue;
-
-		// Having a fingerprint probably means that this is ICE
-		if (msg.fingerprint) {
-			continue;
-		}
-		// Send back a normal binding response
-		else {
-			
-		}
-	}
+	resp.method = msg.method;
+	resp.txid.set(msg.txid);
+	resp.length = 0;
 	
-	console.log(addr, msg.class, msg.method, msg.fingerprint);
-	// for (const {type, value} of msg) {
-	// 	console.log('-', type, value);
+	console.log(addr, msg.class, msg.method, msg.username);
+	// for (const attr of msg) {
+	// 	console.log('-', attr);
 	// }
 }
--- src/stun.js
+++ src/stun.js
@@ -1,7 +1,7 @@
 import { crc32 } from "./crc32.js";
 
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
+export const encoder = new TextEncoder();
+export const decoder = new TextDecoder();
 
 export const Magic = 0x2112A442;
 
@@ -28,13 +28,32 @@ known.set(0x0006, function username(buffer) {
 });
 
 export class Attr extends DataView {
-	#msg;
-	constructor(msg, i, len) {
-		super(msg.buffer, msg.byteOffset + i, len);
-		this.#msg = msg;
+	#parent;
+	constructor(parent, i) {
+		super(parent.buffer, parent.byteOffset + i);
+		this.#parent = parent;
+	}
+	get #header() {
+		return new DataView(this.buffer, this.byteOffset - 4, 4);
+	}
+	get type() {
+		return this.#header.getUint16(0);
+	}
+	set type(value) {
+		this.#header.setUint16(0, value);
+	}
+	get length() {
+		return this.#header.getUint16(2);
+	}
+	set length(value) {
+		this.#parent.length = (this.byteOffset - this.#parent.byteOffset - 20 + value);
+		this.#header.setUint16(2, value);
 	}
 	get prefix() {
-		return new Uint8Array(this.buffer, this.#msg.byteOffset, this.byteOffset - this.#msg.byteOffset - 4);
+		return new Uint8Array(this.buffer, this.#parent.byteOffset, this.byteOffset - this.#parent.byteOffset - 4);
+	}
+	get bytes() {
+		return new Uint8Array(this.buffer, this.byteOffset, this.length);
 	}
 }
 
@@ -72,52 +91,83 @@ export class Stun extends DataView {
 	}
 	set length(len) {
 		while (len % 4 != 0) len += 1;
+		const packet_len = 20 + len;
+		if (packet_len > this.byteLength) this.buffer.resize(this.byteOffset + packet_len);
 		this.setUint16(2, len);
 	}
-	get magic() {
-		return this.getUint32(4);
+	get needed() {
+		if (this.byteLength < 4) return 4;
+		if (this.type < 0x4000) return 20 + this.length;
+		return Infinity;
 	}
-	set magic(magic) {
-		this.setUint32(4, magic);
+	get magic() {
+		return this.getUint32(4) === Magic;
+	}
+	set magic(_) {
+		this.setUint32(4, Magic);
 	}
 	get txid() {
 		return new Uint8Array(this.buffer, this.byteOffset + 4, 16);
 	}
-	*[Symbol.iterator]() {
-		let end = Math.min(20 + this.length, this.byteLength);
-		while (end % 4 != 0) end -= 1;
+	#attrs;
+	get attrs() {
+		if (!this.#attrs) {
+			this.#attrs = new Map();
 
-		for (let i = 20; i < end;) {
-			const attr_typ = this.getUint16(i);
-			const attr_len = this.getUint16(i + 2);
-			if (attr_len > end - i) break;
-			i += 4;
-			const value = new Attr(this, i, attr_len);
-			i += attr_len;
-			while (i % 4 != 0) i += 1;
+			let end = Math.min(20 + this.length, this.byteLength);
+			while (end % 4 != 0) end -= 1;
 
-			// Modify the length as we go
-			this.length = i - 20;
+			for (let i = 20; i < end;) {
+				const attr_typ = this.getUint16(i);
+				const attr_len = this.getUint16(i + 2);
+				if (attr_len > end - i) break;
+				i += 4;
+				const value = new Attr(this, i, attr_len);
+				i += attr_len;
+				while (i % 4 != 0) i += 1;
 
-			yield { type: attr_typ, value };
-		}
-	}
-	add_attr(type, len) {
-
-	}
-	get fingerprint() {
-		for (const {type, value} of this) {
-			if (type == 0x8028) {
-				const actual = value.getInt32(0);
-				const expected = crc32(value.prefix) ^ 0x5354554e;
-
-				return actual === expected;
+				this.#attrs.set(attr_typ, value);
 			}
 		}
-		return undefined;
+		return this.#attrs;
+	}
+	new_attr() {
+		const i = 20 + this.length;
+		this.length += 4;
+		return new Attr(this, i);
+	}
+	get fingerprint() {
+		const attr = this.attrs.get(0x8028);
+		if (!attr) return false;
+		
+		const actual = value.getInt32(0);
+		const expected = crc32(value.prefix) ^ 0x5354554e;
+
+		return actual === expected;
 	}
 	set fingerprint(_) {
-		const value = this.add_attr(0x8028, 4);
-		
+		const a = this.new_attr();
+		a.type = 0x8028;
+		a.length = 4;
+		a.setInt32(crc32(a.prefix) ^ 0x5354554e);
 	}
+	get_txt(type) {
+		const attr = this.attrs.get(type);
+		if (!attr) return undefined;
+		return decoder.decode(attr.bytes);
+	}
+	set_txt(type, value) {
+		const buff = encoder.encode(value);
+		const attr = this.new_attr();
+		attr.type = type;
+		attr.length = buff.byteLength;
+		attr.bytes.set(buff);
+	}
+
+	get username() { return this.get_txt(0x0006); }
+	set username(value) { this.set_txt(0x0006, value); }
+	get realm() { return this.get_txt(0x0014); }
+	set realm(value) { this.set_txt(0x0014, value); }
+	get nonce() { return this.get_txt(0x0015); }
+	set nonce(value) { this.set_txt(0x0015, value); }
 }
