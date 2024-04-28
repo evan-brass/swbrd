8f43527984e43eeda929d99ba202bb0cc4f0a141
Evan Brass <brassevan@gmail.com>
2024-04-28 12:07:07 -0700

    rewrite base
    Still not compatible with btoa and also this invalidates all existing ids (or at least the string encodings of those ids).
    (I mean, I don't really understand this code, but I think I just reordered the characters to put most significant char first.)

--- src/base.js
+++ src/base.js
@@ -3,6 +3,11 @@ export const base62 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234
 export const base64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 export const urlbase64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
 
+// base(0xdeadbeef01234567n, {charset: base64}) == 'N6tvu8BI0Vn'
+// btoa(String.fromCharCode(0xde,0xad,0xbe,0xef,0x01,0x23,0x45,0x67)) == '3q2+7wEjRWc='
+// Honestly, I don't really know why they are completely different.  I must not understand how base64 works.
+// My current guess is that it has something to do with my implementation encoding numbers instead of bytes.
+
 export function base(input, {
 	charset = base62,
 	bits = 256,
@@ -16,15 +21,15 @@ export function base(input, {
 			ret ??= 0n;
 			const d = charset.indexOf(input[i]);
 			if (d == -1) return;
-			ret += BigInt(charset.length) ** BigInt(i) * BigInt(d);
+			ret += BigInt(d) * BigInt(charset.length) ** BigInt(input.length - i - 1);
 		}
-		if (BigInt.asUintN(bits, ret) != ret) return;
+		if (BigInt.asUintN(bits, ret) != ret) return; // Make sure that the result is within the numbers representable with the given number of bits
 	}
 	else {
 		input = BigInt(input);
 		while (input > 0n) {
 			ret ??= '';
-			ret += charset.charAt(Number(input % BigInt(charset.length)));
+			ret = charset.charAt(Number(input % BigInt(charset.length))) + ret;
 			input /= BigInt(charset.length);
 		}
 		if (pad_len) ret.padStart(pad_len, charset[0]);
