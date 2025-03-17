f6ec2fbe420c16917931045e3324da6d284fef66
Evan Brass <brassevan@gmail.com>
2025-03-17 08:47:00 -0700

    _

--- c-src/buffer_access.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <mbedtls/x509_crt.h>
-
-size_t get_x509_crt_len(mbedtls_x509_crt* cert) {
-	return cert->raw.len;
-}
-unsigned char* get_x509_crt_ptr(mbedtls_x509_crt* cert) {
-	return cert->raw.p;
-}
