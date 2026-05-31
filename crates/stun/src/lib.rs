cf4f42eb1467eb6a008cd1687f394599234341b1
Evan Brass <brassevan@gmail.com>
2026-05-31 15:12:23 -0700

    _

--- crates/stun/src/lib.rs
+++ crates/stun/src/lib.rs
@@ -1,1 +1,43 @@
 #![no_std]
+
+use core::{cell::Cell, marker::PhantomData, mem::offset_of};
+
+use zerocopy::{
+	FromBytes, Immutable, IntoBytes, KnownLayout, SplitAt, TryFromBytes, Unaligned,
+	network_endian::U16,
+};
+
+pub use crate::typ::*;
+
+mod typ;
+
+#[derive(Debug)]
+pub struct Iterating;
+
+#[repr(C)]
+#[derive(Debug, PartialEq, Eq, KnownLayout, Unaligned, TryFromBytes, IntoBytes, SplitAt)]
+pub struct Stun<M = ()> {
+	iter: PhantomData<M>,
+	opad: [u8; 64],
+	ipad: [u8; 64],
+	pub class: Class,
+	pub method: Method,
+	pub(crate) length: Cell<U16>,
+	pub txid: Txid,
+	pub(crate) body: [[u8; 4]],
+}
+impl Stun<()> {
+	pub const HEADROOM: usize = offset_of!(Self, class);
+	pub const MAX_LENGTH: u16 = 0xff00;
+}
+
+#[repr(C, packed)]
+#[derive(KnownLayout, Immutable, Unaligned, FromBytes, IntoBytes)]
+pub struct Attr {
+	pub typ: u16,
+	length: U16,
+	pub value: [u8],
+}
+impl Attr {
+	pub const MAX_LENGTH: u16 = Stun::MAX_LENGTH - 4;
+}
---
+++ crates/stun/src/typ.rs
@@ -0,0 +1,78 @@
+use core::num::NonZero;
+
+use zerocopy::{Immutable, IntoBytes, KnownLayout, TryFromBytes, Unaligned, try_transmute};
+
+#[repr(u8)]
+#[derive(
+	Debug, Clone, Copy, PartialEq, Eq, KnownLayout, Immutable, Unaligned, TryFromBytes, IntoBytes,
+)]
+pub enum Class {
+	Request = 0x00,
+	Response = 0x01,
+}
+
+#[repr(u8)]
+#[derive(
+	Debug, Clone, Copy, PartialEq, Eq, KnownLayout, Immutable, Unaligned, TryFromBytes, IntoBytes,
+)]
+#[non_exhaustive]
+pub enum Method {
+	Bind = 0x01,
+	Allocate = 0x03,
+	Refresh = 0x04,
+	Send = 0x16,
+	Recv = 0x17,
+	AddPermission = 0x08,
+	UseChannel = 0x09,
+
+	// Don't use these:
+	// As a client, treat any response.method != request.method as an error
+	// As a server, use .to_err to get these variants
+	#[doc(hidden)]
+	Fuck = 0x11, // Bind Error or Bind Indication
+	#[doc(hidden)]
+	#[deprecated]
+	This = 0x13, // Allocate Error
+	#[doc(hidden)]
+	#[deprecated]
+	Shit = 0x14, // Refresh Error
+	#[doc(hidden)]
+	#[deprecated]
+	So = 0x18, // AddPermission Error
+	#[doc(hidden)]
+	#[deprecated]
+	Ass = 0x19, // UseChannel Error
+}
+impl Method {
+	pub fn to_err(self) -> Self {
+		try_transmute!(self as u8 ^ 0x10).unwrap()
+	}
+}
+
+#[repr(u32)]
+#[derive(
+	Default, Debug, Clone, Copy, PartialEq, Eq, KnownLayout, Immutable, TryFromBytes, IntoBytes,
+)]
+pub enum Cookie {
+	#[default]
+	Magic = u32::to_be(0x2112_A442),
+}
+
+#[repr(C, packed)]
+#[derive(
+	Debug, Clone, Copy, PartialEq, Eq, KnownLayout, Immutable, Unaligned, TryFromBytes, IntoBytes,
+)]
+pub struct Txid {
+	cookie: Cookie,
+	pub id: [NonZero<u8>; 12],
+}
+impl Txid {
+	pub fn new() -> Self {
+		// TODO: Randomize id
+		let id = unsafe { [NonZero::new_unchecked(0x99); 12] };
+		Self {
+			cookie: Cookie::Magic,
+			id,
+		}
+	}
+}
