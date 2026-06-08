aad55f17de19e1e991e1ca33c7b42fe93ead6fa7
Evan Brass <brassevan@gmail.com>
2026-06-08 11:28:46 -0700

    Prep for tcp

--- crates/turnserver/src/main.rs
+++ crates/turnserver/src/main.rs
@@ -109,7 +109,34 @@ fn main() -> Result<()> {
 	let tun_handler = ThreadBuilder::new().name("TUN listener".into());
 
 	scope(|s| -> Result<()> {
-		turn_handler.spawn_scoped(s, || handle_turn(&mappings, &socket, &network))?;
+		turn_handler.spawn_scoped(s, || {
+			let mut buffer = vec![0; 65536];
+
+			// Preload the authkey into the buffer.  As long as we never touch bellow the HEADROOM of the buffer then this data will stay in place
+			let t = Stun::new(Class::Request, Method::Bind, &mut buffer).unwrap();
+			t.set_authkey(TURNKEY);
+
+			loop {
+				let Ok((20.., SocketAddr::V6(sender))) =
+					socket.recv_from(&mut buffer[Stun::HEADROOM..])
+				else {
+					continue;
+				};
+				let Ok(msg) = Stun::try_mut_from_bytes(&mut buffer) else {
+					continue;
+				};
+				if msg.class != Class::Request || msg.method == Method::Recv || msg.method.is_err()
+				{
+					continue;
+				}
+				if let Some(resp) = handle_turn(&mappings, sender, msg, &network) {
+					let end = size_of_val(resp.trim());
+					socket
+						.send_to(&buffer[Stun::HEADROOM..end], sender)
+						.unwrap();
+				}
+			}
+		})?;
 		tun_handler.spawn_scoped(s, || handle_tun(&mappings, &socket, &network))?;
 
 		Ok(())
@@ -118,191 +145,174 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
-fn handle_turn(mappings: &Vec<Mapping>, socket: &UdpSocket, network: &SyncDevice) {
-	let mut buffer = vec![0; 65536];
-
-	// Preload the authkey into the buffer.  As long as we never touch bellow the HEADROOM of the buffer then this data will stay in place
-	let t = Stun::new(Class::Request, Method::Bind, &mut buffer).unwrap();
-	t.set_authkey(TURNKEY);
-
-	loop {
-		let Ok((20.., SocketAddr::V6(sender))) = socket.recv_from(&mut buffer[Stun::HEADROOM..])
-		else {
-			continue;
-		};
-		let Ok(msg) = Stun::try_mut_from_bytes(&mut buffer) else {
-			continue;
-		};
-		if msg.class != Class::Request || msg.method == Method::Recv || msg.method.is_err() {
-			continue;
+fn handle_turn<'i>(
+	mappings: &Vec<Mapping>,
+	sender: SocketAddrV6,
+	msg: &'i mut Stun,
+	network: &SyncDevice,
+) -> Option<&'i mut Stun> {
+	// Apply our subnet mappings to the sender/source ip
+	let mut src_ip = *sender.ip();
+	for m in mappings {
+		if m.to_net(&mut src_ip) {
+			break;
 		}
+	}
 
-		// Apply our subnet mappings to the sender/source ip
-		let mut src_ip = *sender.ip();
-		for m in mappings {
-			if m.to_net(&mut src_ip) {
+	let mut username = Parsed::NotPresent;
+	let mut software = Parsed::NotPresent;
+	let mut channel = Parsed::NotPresent;
+	let mut lifetime = Parsed::NotPresent;
+	let mut peer = Parsed::NotPresent;
+	let mut data = Parsed::NotPresent;
+	let mut realm = Parsed::NotPresent;
+	let mut nonce = Parsed::NotPresent;
+	let mut transport = Parsed::NotPresent;
+	let mut integrity = false;
+
+	let attrs = msg
+		.trim()
+		.parse::<{ known::USERNAME }, str>(&mut username)
+		.parse::<{ known::SOFTWARE }, str>(&mut software)
+		.parse::<{ known::CHANNEL_NUMBER }, [u8; 4]>(&mut channel)
+		.parse::<{ known::LIFETIME }, U32>(&mut lifetime)
+		.parse::<{ known::XOR_PEER_ADDRESS }, Addr6<Xor>>(&mut peer)
+		.parse::<{ known::DATA }, [u8]>(&mut data)
+		.parse::<{ known::REALM }, str>(&mut realm)
+		.parse::<{ known::NONCE }, str>(&mut nonce)
+		.parse::<{ known::REQUESTED_TRANSPORT }, [u8; 4]>(&mut transport);
+
+	msg.length.set(U16::new(0));
+	for (prefix, attr) in attrs {
+		match attr.typ {
+			known::MESSAGE_INTEGRITY => {
+				integrity = attr.value == prefix.expected_message_integrity();
 				break;
 			}
+			_ if attr.is_optional() => {}
+			t => todo!("Unknown Attributes error response {t:?}"),
 		}
+	}
 
-		let mut username = Parsed::NotPresent;
-		let mut software = Parsed::NotPresent;
-		let mut channel = Parsed::NotPresent;
-		let mut lifetime = Parsed::NotPresent;
-		let mut peer = Parsed::NotPresent;
-		let mut data = Parsed::NotPresent;
-		let mut realm = Parsed::NotPresent;
-		let mut nonce = Parsed::NotPresent;
-		let mut transport = Parsed::NotPresent;
-		let mut integrity = false;
+	let lifetime = if let Parsed::Valid(l) = lifetime {
+		*l
+	} else {
+		U32::new(60_000)
+	};
 
-		let attrs = msg
-			.trim()
-			.parse::<{ known::USERNAME }, str>(&mut username)
-			.parse::<{ known::SOFTWARE }, str>(&mut software)
-			.parse::<{ known::CHANNEL_NUMBER }, [u8; 4]>(&mut channel)
-			.parse::<{ known::LIFETIME }, U32>(&mut lifetime)
-			.parse::<{ known::XOR_PEER_ADDRESS }, Addr6<Xor>>(&mut peer)
-			.parse::<{ known::DATA }, [u8]>(&mut data)
-			.parse::<{ known::REALM }, str>(&mut realm)
-			.parse::<{ known::NONCE }, str>(&mut nonce)
-			.parse::<{ known::REQUESTED_TRANSPORT }, [u8; 4]>(&mut transport);
-
-		msg.length.set(U16::new(0));
-		for (prefix, attr) in attrs {
-			match attr.typ {
-				known::MESSAGE_INTEGRITY => {
-					integrity = attr.value == prefix.expected_message_integrity();
-					break;
-				}
-				_ if attr.is_optional() => {}
-				t => todo!("Unknown Attributes error response {t:?}"),
-			}
-		}
-
-		let lifetime = if let Parsed::Valid(l) = lifetime {
-			*l
-		} else {
-			U32::new(60_000)
-		};
-
-		let add_mapped = move |msg: &mut Stun| match sender.ip().to_canonical() {
-			IpAddr::V4(v4) => {
-				msg.append_val(
-					known::XOR_MAPPED_ADDRESS,
-					&Addr4::new(v4, sender.port()).xor(&msg.txid),
-				);
-			}
-			IpAddr::V6(v6) => {
-				msg.append_val(
-					known::XOR_MAPPED_ADDRESS,
-					&Addr6::new(v6, sender.port()).xor(&msg.txid),
-				);
-			}
-		};
-
-		match msg.method {
-			Method::Bind => {
-				msg.class = Class::Response;
-				msg.length.get_mut().set(0);
-				add_mapped(msg);
-			}
-			Method::Send => {
-				let (Parsed::Valid(peer), Parsed::Valid(data)) = (peer, data) else {
-					continue;
-				};
-				let peer = peer.xor(&msg.txid);
-				let ip = Ip6 {
-					flags: Ip6::FLAGS,
-					length: U16::new((size_of::<Udp>() + data.len()) as u16),
-					next_header: 17,
-					hop_limit: 64,
-					src: src_ip.octets(),
-					dst: peer.ip().octets(),
-				};
-				let mut udp = Udp {
-					src_port: U16::new(sender.port()),
-					dst_port: U16::new(peer.port()),
-					length: ip.length,
-					checksum: 0,
-				};
-
-				if cfg!(target_os = "linux") {
-					let vnet = partial_checksum(&ip, &mut udp);
-					network.send_vectored(&[
-						IoSlice::new(vnet.as_bytes()),
-						IoSlice::new(ip.as_bytes()),
-						IoSlice::new(udp.as_bytes()),
-						IoSlice::new(data),
-					])
-				} else {
-					full_checksum(&ip, &mut udp, data);
-					network.send_vectored(&[
-						IoSlice::new(ip.as_bytes()),
-						IoSlice::new(udp.as_bytes()),
-						IoSlice::new(data),
-					])
-				}
-				.unwrap();
-
-				continue;
-			}
-			m if realm != Parsed::Valid("none") => {
-				msg.class = Class::Response;
-				msg.method = m.to_err();
-				msg.length.get_mut().set(0);
-				msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 1]);
-				msg.append_val(known::REALM, "none");
-				msg.append_val(known::NONCE, "none");
-			}
-			m if (nonce, integrity) != (Parsed::Valid("none"), true) => {
-				msg.class = Class::Response;
-				msg.method = m.to_err();
-				msg.length.get_mut().set(0);
-				msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 3]);
-			}
-			Method::Allocate => {
-				msg.class = Class::Response;
-				msg.length.get_mut().set(0);
-				add_mapped(msg);
-				msg.append_val(
-					known::XOR_RELAYED_ADDRESS,
-					&Addr6::new(src_ip, sender.port()).xor(&msg.txid),
-				);
-				msg.append_val(known::LIFETIME, &lifetime);
-			}
-			// Close notification, just drop
-			Method::Refresh if lifetime.get() == 0 => continue,
-			Method::Refresh => {
-				msg.class = Class::Response;
-				msg.length.get_mut().set(0);
-				msg.append_val(known::LIFETIME, &lifetime);
-			}
-			Method::AddPermission => {
-				msg.class = Class::Response;
-				msg.length.get_mut().set(0);
-			}
-			Method::UseChannel => {
-				msg.class = Class::Response;
-				msg.method = msg.method.to_err();
-				msg.length.get_mut().set(0);
-				msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 38]);
-			}
-			_ => continue,
-		}
-
-		if integrity {
+	let add_mapped = move |msg: &mut Stun| match sender.ip().to_canonical() {
+		IpAddr::V4(v4) => {
 			msg.append_val(
-				known::MESSAGE_INTEGRITY,
-				&msg.trim().expected_message_integrity(),
+				known::XOR_MAPPED_ADDRESS,
+				&Addr4::new(v4, sender.port()).xor(&msg.txid),
 			);
 		}
+		IpAddr::V6(v6) => {
+			msg.append_val(
+				known::XOR_MAPPED_ADDRESS,
+				&Addr6::new(v6, sender.port()).xor(&msg.txid),
+			);
+		}
+	};
 
-		let end = size_of_val(msg.trim());
-		socket
-			.send_to(&buffer[Stun::HEADROOM..end], sender)
+	match msg.method {
+		Method::Bind => {
+			msg.class = Class::Response;
+			msg.length.get_mut().set(0);
+			add_mapped(msg);
+		}
+		Method::Send => {
+			let (Parsed::Valid(peer), Parsed::Valid(data)) = (peer, data) else {
+				return None;
+			};
+			let peer = peer.xor(&msg.txid);
+			let ip = Ip6 {
+				flags: Ip6::FLAGS,
+				length: U16::new((size_of::<Udp>() + data.len()) as u16),
+				next_header: 17,
+				hop_limit: 64,
+				src: src_ip.octets(),
+				dst: peer.ip().octets(),
+			};
+			let mut udp = Udp {
+				src_port: U16::new(sender.port()),
+				dst_port: U16::new(peer.port()),
+				length: ip.length,
+				checksum: 0,
+			};
+
+			if cfg!(target_os = "linux") {
+				let vnet = partial_checksum(&ip, &mut udp);
+				network.send_vectored(&[
+					IoSlice::new(vnet.as_bytes()),
+					IoSlice::new(ip.as_bytes()),
+					IoSlice::new(udp.as_bytes()),
+					IoSlice::new(data),
+				])
+			} else {
+				full_checksum(&ip, &mut udp, data);
+				network.send_vectored(&[
+					IoSlice::new(ip.as_bytes()),
+					IoSlice::new(udp.as_bytes()),
+					IoSlice::new(data),
+				])
+			}
 			.unwrap();
+
+			return None;
+		}
+		m if realm != Parsed::Valid("none") => {
+			msg.class = Class::Response;
+			msg.method = m.to_err();
+			msg.length.get_mut().set(0);
+			msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 1]);
+			msg.append_val(known::REALM, "none");
+			msg.append_val(known::NONCE, "none");
+		}
+		m if (nonce, integrity) != (Parsed::Valid("none"), true) => {
+			msg.class = Class::Response;
+			msg.method = m.to_err();
+			msg.length.get_mut().set(0);
+			msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 3]);
+		}
+		Method::Allocate => {
+			msg.class = Class::Response;
+			msg.length.get_mut().set(0);
+			add_mapped(msg);
+			msg.append_val(
+				known::XOR_RELAYED_ADDRESS,
+				&Addr6::new(src_ip, sender.port()).xor(&msg.txid),
+			);
+			msg.append_val(known::LIFETIME, &lifetime);
+		}
+		// Close notification, just drop
+		Method::Refresh if lifetime.get() == 0 => return None,
+		Method::Refresh => {
+			msg.class = Class::Response;
+			msg.length.get_mut().set(0);
+			msg.append_val(known::LIFETIME, &lifetime);
+		}
+		Method::AddPermission => {
+			msg.class = Class::Response;
+			msg.length.get_mut().set(0);
+		}
+		Method::UseChannel => {
+			msg.class = Class::Response;
+			msg.method = msg.method.to_err();
+			msg.length.get_mut().set(0);
+			msg.append_val(known::ERROR_CODE, &[0u8, 0, 4, 38]);
+		}
+		_ => return None,
 	}
+
+	if integrity {
+		msg.append_val(
+			known::MESSAGE_INTEGRITY,
+			&msg.trim().expected_message_integrity(),
+		);
+	}
+
+	Some(msg)
 }
 
 fn handle_tun(mappings: &Vec<Mapping>, socket: &UdpSocket, network: &SyncDevice) {
